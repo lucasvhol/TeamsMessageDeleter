1ce764ca85c6fae7a5215fa6b911bdc4219a0e30
Lucas H <75590817+lucasvhol@users.noreply.github.com>
2022-11-24 15:55:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,90 @@
-(function() {
-	var JQ = jQuery("iframe").contents();
-	var queue = [];
-	var last_processed = (new Date()).getTime();
-	var loading = true;
-	var my_name = null;
-	var mouse = function(which, element) {
-		var evt = element.ownerDocument.createEvent('MouseEvents');
-		
-		evt.initMouseEvent(which, true, true,
-			element.ownerDocument.defaultView, 1, 0, 0, 0, 0, false,
-			false, false, false, 1, null);
-		
-		element.dispatchEvent(evt);
-	}
+ (function() {
+     var JQ = jQuery("iframe").contents();
+     var queue = [];
+     var last_processed = (new Date()).getTime();
+     var loading = true;
+     var my_name = null;
 
-	var repeat = setInterval(function() {
-		if(loading) {
-			if(JQ.find("div.pending").size() == 0) {
-				loading = false;
-				JQ.find('div[data-mid]').each(function() {
-					var id = $(this).attr('data-mid');
-					if(isNaN(id)) return;
+     var mouse = function(which, element) {
+         var evt = element.ownerDocument.createEvent('MouseEvents');
 
-					if($(this).find("[data-tid=message-undo-delete-btn]").size() > 0) return;
+         evt.initMouseEvent(which, true, true,
+             element.ownerDocument.defaultView, 1, 0, 0, 0, 0, false,
+             false, false, false, 1, null);
 
-					queue.push(this);
-					$(this).css({'outline': '5px solid #000'});
-				});
-			}
-			else return;
-		}
+         element.dispatchEvent(evt);
+     }
 
-		if(queue.length > 0) {
-			var item = queue.shift();
-			if(my_name && my_name != $(item).find("[data-tid=message-author-name]").text()) return;
+     var repeat = setInterval(function() {
 
-			$(item).css({'outline': '5px solid #cc0'});
-			console.log("Delete ID " + $(item).attr("data-mid"));
-			mouse('contextmenu', $(item).find(".ui-chat__messagecontent>div")[0]);
-			setTimeout(function() {
-				var delete_button = JQ.find("[data-tid=message-actions-delete]")[0];
-				if(delete_button) {
-					my_name = $(item).find("[data-tid=message-author-name]").text();
-					console.log(["My name is: ", my_name]);
-					mouse('click', delete_button);
-				}
-			}, 100);
+         if (loading) {
 
-			last_processed = (new Date()).getTime();
-		}
-		else {
-			JQ.find('[data-tid=message-pane-list-viewport]').scrollTop(0);
-			loading = true;
-		}
-		
-		if(last_processed < (new Date()).getTime() - 30000) {
-			// if no new messages are coming up for 30s, consider it finished
-			clearInterval(repeat);
-			console.log("Finished, nothing new has come up for 30 seconds.");
-		}
-	
-	}, 1000);
-})();
+             if (JQ.find("div.pending").size() == 0) {
+                 loading = false;
+                 $(JQ.find('div[data-mid]').get().reverse()).each(function() {
+                     var id = $(this).attr('data-mid');
+                     if (isNaN(id)) return;
+
+                     if ($(this).find("[data-tid=message-undo-delete-btn]").size() > 0) return;
+
+                     queue.push(this);
+                     $(this).css({
+                         'outline': '5px solid #blue'
+                     });
+                 });
+             } else return;
+         }
+
+
+         while (true) {
+             if (queue.length > 0) {
+                 var item = queue.shift();
+                 if (my_name && my_name != $(item).find("[data-tid=message-author-name]").text()) {
+                     $(item).css({
+                         'outline': '5px solid red'
+                     });
+                 } else {
+                     $(item).css({
+                         'outline': '5px solid blue'
+                     });
+                     mouse('contextmenu', $(item).find(".ui-chat__messagecontent>div")[0]);
+
+                     var delete_button = JQ.find("[data-tid=message-actions-delete]")[0];
+                     if (delete_button) {
+                         console.log("Delete ID " + $(item).attr("data-mid"));
+                         $(item).css({
+                             'outline': '5px solid green'
+                         });
+                         my_name = $(item).find("[data-tid=message-author-name]").text();
+                         console.log(["My name is: ", my_name]);
+                         mouse('click', delete_button);
+                         last_processed = (new Date()).getTime();
+                         break;
+                     } else {
+                         $(item).css({
+                             'outline': '5px solid red'
+                         });
+                     }
+                 }
+
+                 setTimeout(function() {
+                     $(item)[0].scrollIntoView(false);
+                 }, 100);
+
+             } else
+                 break;
+
+         }
+
+         if (queue.length <= 0) {
+             loading = true;
+
+         }
+
+         if (last_processed < (new Date()).getTime() - 30000) {
+             // if no new messages are coming up for 15s, consider it finished
+             clearInterval(repeat);
+             console.log("Finished, nothing new has come up for 30 seconds.");
+         }
+     }, 1000);
+ })();
